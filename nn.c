b5bd15e0a3750f014f072d1d5070728fe62b8101
Wink Saville <wink@saville.com>
2016-11-15 23:00:34 -0800

    More debug and is matching test-nn outputs.
    In test-nn I use structures instead of straight arrays hence
    I use different names:
    DeltaO & DeltaH == pd_error
    DeltaWeightIH & DeltaWeightHO == momentums
    WeightIH & WeightHO == weights
    Hidden & Output == output

--- nn.c
+++ nn.c
@@ -18,10 +18,11 @@
 
 #define rando() ((double)rand()/((double)RAND_MAX+1))
 
-#if 1
-#define dbg(...) fprintf(stdout, __VA_ARGS__)
-#else
+#define NDEBUG 0
+#if NDEBUG
 #define dbg(...) ((void*)0)
+#else
+#define dbg(...) fprintf(stdout, __VA_ARGS__)
 #endif
 
 int main(void) {
@@ -65,7 +66,7 @@ int main(void) {
         }
 #endif
         Error = 0.0 ;
-        double sse, diff ;
+        double sse, err ;
         for( np = 1 ; np <= NumPattern ; np++ ) {    /* repeat for all the training patterns */
             p = ranpat[np];
             for ( i = 1 ; i <= NumInput ; i++ ) {
@@ -88,14 +89,14 @@ int main(void) {
                 dbg("%d:%d Target=%lf Output=%lf SumO=%lf\n",
                     p, k, Target[p][k], Output[p][k], SumO[p][k]);
 /*              Output[p][k] = SumO[p][k];      Linear Outputs */
-                diff = Target[p][k] - Output[p][k] ;
-                sse = 0.5 * diff * diff ;   /* SSE */
+                err = Target[p][k] - Output[p][k] ;
+                sse = 0.5 * err * err ;   /* SSE */
                 Error += sse;
 /*              Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) ) ;    Cross-Entropy Error */
                 DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]) ;   /* Sigmoidal Outputs, SSE */
 /*              DeltaO[k] = Target[p][k] - Output[p][k];     Sigmoidal Outputs, Cross-Entropy Error */
 /*              DeltaO[k] = Target[p][k] - Output[p][k];     Linear Outputs, SSE */
-                dbg("%d:%d diff=%lf, sse=%lf DeltaO[%d]=%lf\n", p, k, diff, sse, k, DeltaO[k]);
+                dbg("%d:%d err=%lf, DeltaO[%d]=%lf sse=%lf\n", p, k, err, k, DeltaO[k], sse);
             }
             dbg("back-propagate errors:\n");
             for( j = 1 ; j <= NumHidden ; j++ ) {    /* 'back-propagate' errors to hidden layer */
@@ -109,58 +110,68 @@ int main(void) {
                 dbg("DeltaH[%d]:%lf = SumDOW[%d]:%lf * Hidden[%d][%d]:%lf * (1.0 - Hidden[%d][%d]:%lf\n",
                      j, DeltaH[j], j, SumDOW[j], p, j, Hidden[p][j], p, j, Hidden[p][j]);
             }
-            dbg("Update WeightsIH:\n");
+            dbg("Update WeightsIH: eta=%lf alpha=%lf\n", eta, alpha);
             for( j = 1 ; j <= NumHidden ; j++ ) {     /* update weights WeightIH */
-#if 0
+#if NDEBUG
                 DeltaWeightIH[0][j] = eta * DeltaH[j] + alpha * DeltaWeightIH[0][j] ;
+                WeightIH[0][j] +=  DeltaWeightIH[0][j] ;
 #else
-                double DWIH = DeltaWeightIH[0][j] ;
-                DeltaWeightIH[0][j] = eta * DeltaH[j] + alpha * DWIH ;
-                dbg("DeltaWeightIH[%d][%d]:%lf = eta:%lf * DeltaH[%d]:%lf + alpha:%lf * DWIH:%lf BIAS\n",
-                     0, j, DeltaWeightIH[0][j], eta, j, DeltaH[j], alpha, DWIH);
+                double momentum = alpha * DeltaWeightIH[0][j] ;
+                DeltaWeightIH[0][j] = (eta * DeltaH[j]) + momentum ;
+                dbg("DeltaWeightIH[%d][%d]:%lf = (eta:%lf * DeltaH[%d]:%lf) + momentum:%lf BIAS\n",
+                     0, j, DeltaWeightIH[0][j], eta, j, DeltaH[j], momentum);
 #endif
-                WeightIH[0][j] += DeltaWeightIH[0][j] ;
-                dbg("WeightIH[%d][%d]:%lf += DeltaWeightIH[%d][%d]:%lf\n",
-                     0, j, WeightIH[0][j], 0, j, DeltaWeightIH[0][j]);
+                double w = WeightIH[0][j];
+                WeightIH[0][j] = WeightIH[0][j] + DeltaWeightIH[0][j] ;
+                dbg("WeightIH[%d][%d]:%lf = WeightIH[%d][%d]:%lf + DeltaWeightIH[%d][%d]:%lf\n",
+                     0, j, WeightIH[0][j], 0, j, w, 0, j, DeltaWeightIH[0][j]);
                 for( i = 1 ; i <= NumInput ; i++ ) { 
-#if 0
+#if NDEBUG
                     DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j] + alpha * DeltaWeightIH[i][j];
+                    WeightIH[i][j] +=  DeltaWeightIH[i][j] ;
 #else
-                    DWIH = DeltaWeightIH[i][j];
-                    DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j] + alpha * DWIH;
-                    dbg("DeltaWeightIH[%d][%d]:%lf = eta:%lf * Input[%d][%i]:%lf * DeltaH[%d]:%lf + alpha:%lf * DWIH:%lf\n",
-                         i, j, DeltaWeightIH[i][j], eta, j, p, i, Input[p][i], DeltaH[j], alpha, DWIH);
+                    momentum = alpha * DeltaWeightIH[i][j];
+                    DeltaWeightIH[i][j] = (eta * Input[p][i] * DeltaH[j]) + momentum;
+                    dbg("DeltaWeightIH[%d][%d]:%lf = (eta:%lf * Input[%d][%i]:%lf * DeltaH[%d]:%lf) + momentum:%lf\n",
+                         i, j, DeltaWeightIH[i][j], eta, j, p, i, Input[p][i], DeltaH[j], momentum);
+                    w = WeightIH[i][j];
+                    WeightIH[i][j] =  WeightIH[i][j] + DeltaWeightIH[i][j] ;
+                    dbg("WeightIH[%d][%d]:%lf = WeightIH[%d][%d]:%lf + DeltaWeightIH[%d][%d]:%lf\n",
+                         i, j, WeightIH[i][j], i, j, w, i, j, DeltaWeightIH[i][j]);
 #endif
-                    WeightIH[i][j] += DeltaWeightIH[i][j] ;
-                    dbg("WeightIH[%d][%d]:%lf += DeltaWeightIH[%d][%d]:%lf\n",
-                         i, j, WeightIH[0][j], i, j, DeltaWeightIH[0][j]);
                 }
             }
             dbg("Update WeightsHO:\n");
             for( k = 1 ; k <= NumOutput ; k ++ ) {    /* update weights WeightHO */
-#if 0
+#if NDEBUG
                 DeltaWeightHO[0][k] = eta * DeltaO[k] + alpha * DeltaWeightHO[0][k] ;
-#else
-                double DWHO = DeltaWeightHO[0][k];
-                DeltaWeightHO[0][k] = eta * DeltaO[k] + alpha * DWHO;
-                dbg("DeltaWeightHO[%d][%d]:%lf = eta:%lf * DeltaO[%d]:%lf + alpha:%lf * DWHO:%lf BIAS\n",
-                     0, k, DeltaWeightIH[0][k], eta, k, DeltaH[k], alpha, DWHO);
-#endif
                 WeightHO[0][k] += DeltaWeightHO[0][k] ;
-                dbg("WeightHO[%d][%d]:%lf += DeltaWeightHO[%d][%d]:%lf\n",
-                     0, k, WeightHO[0][k], 0, j, DeltaWeightHO[0][k]);
-                for( j = 1 ; j <= NumHidden ; j++ ) {
-#if 0
-                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DeltaWeightHO[j][k] ;
 #else
-                    DWHO = DeltaWeightHO[j][k];
-                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DWHO ;
-                    dbg("DeltaWeightHO[%d][%d]:%lf = eta:%lf * Hidden[%d][%d]:%lf * DeltaO[%d]:%lf + alpha:%lf * DWHO:%lf\n",
-                         j, k, DeltaWeightHO[j][k], eta, p, j, Hidden[p][j], k, DeltaO[k], alpha, DWHO);
+                double momentum = alpha * DeltaWeightHO[0][k];
+                printf("momentum:%lf = alpha:%lf DeltaWeightHO[%d][%d]:%lf\n",
+                    momentum, alpha, 0, k, DeltaWeightHO[0][k]);
+                DeltaWeightHO[0][k] = (eta * DeltaO[k]) + momentum;
+                dbg("DeltaWeightHO[%d][%d]:%lf = (eta:%lf * DeltaO[%d]:%lf) + momentum:%lf BIAS\n",
+                     0, k, DeltaWeightHO[0][k], eta, k, DeltaO[k], momentum);
+                double w = WeightHO[0][k];
+                WeightHO[0][k] = WeightHO[0][k] + DeltaWeightHO[0][k] ;
+                dbg("WeightHO[%d][%d]:%lf = WeightHO[%d][%d] + DeltaWeightHO[%d][%d]:%lf\n",
+                     0, k, WeightHO[0][k], 0, k, w, 0, k, DeltaWeightHO[0][k]);
 #endif
+                for( j = 1 ; j <= NumHidden ; j++ ) {
+#if NDEBUG
+                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DeltaWeightHO[j][k] ;
                     WeightHO[j][k] += DeltaWeightHO[j][k] ;
-                    dbg("WeightHO[%d][%d]:%lf += DeltaWeightHO[%d][%d]:%lf\n",
-                         j, k, WeightHO[j][k], j, k, DeltaWeightHO[j][k]);
+#else
+                    momentum = alpha * DeltaWeightHO[j][k];
+                    DeltaWeightHO[j][k] = (eta * Hidden[p][j] * DeltaO[k]) + momentum ;
+                    dbg("DeltaWeightHO[%d][%d]:%lf = (eta:%lf * Hidden[%d][%d]:%lf * DeltaO[%d]:%lf) + momentum:%lf\n",
+                         j, k, DeltaWeightHO[j][k], eta, p, j, Hidden[p][j], k, DeltaO[k], momentum);
+                    w = WeightHO[j][k];
+                    WeightHO[j][k] = WeightHO[j][k] + DeltaWeightHO[j][k] ;
+                    dbg("WeightHO[%d][%d]:%lf = WeightHO[%d][%d]:%lf +  DeltaWeightHO[%d][%d]:%lf\n",
+                         j, k, WeightHO[j][k], j, k, w, j, k, DeltaWeightHO[j][k]);
+#endif
                 }
             }
         }
